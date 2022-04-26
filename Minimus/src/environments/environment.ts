313c63168dfa494dffb4c8271c557f4620a28052
dangduytung <demontendote@gmail.com>
2022-04-26 10:31:32 +0700

    Upload project Minimus to branch test

---
+++ Minimus/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  appID: '421612f4c4fd7e87a2399806a781659c',
+  gKey: '',
+  config: {
+    apiKey: 'AIzaSyBd6TjUAtmw1PHeYVkY-OWWqprUA0d4gsM',
+    authDomain: 'localtest-143215.firebaseapp.com',
+    databaseURL: 'https://localtest-143215.firebaseio.com',
+    projectId: 'localtest-143215',
+    storageBucket: 'localtest-143215.appspot.com',
+    messagingSenderId: '329147897306'
+  }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
